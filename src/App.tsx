b009d9d7bbe92d193dede25d6f338d83be27ea4c
YouKnowPrem <thepremsingh2111@gmail.com>
2026-05-31 01:42:24 -0600

    Scrap the UAS page by deleting UAS.tsx and removing its imports and routes from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
-import { UAS } from './pages/UAS';
 import { SecurityParams } from './pages/SecurityParams';
 import { Tactical } from './pages/Tactical';
 import { Aerospace } from './pages/Aerospace';
@@ -57,7 +56,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/uas" element={<UAS />} />
             <Route path="/security" element={<SecurityParams />} />
             <Route path="/tactical" element={<Tactical />} />
             <Route path="/aerospace" element={<Aerospace />} />
